ef958a8601b13df35300c8af1c553630e5de9fda
Q118 <shelbyfish91@gmail.com>
2020-08-04 17:58:13 -0700

    set up event listeners for service-workers

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,27 +1,99 @@
+/** @format */
+
 console.log("Hey from service worker!");
 
 //set up cache files
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/index.js",
-    "/manifest.webmanifest",
-    "/styles.css",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png",
-    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
-
+	"/",
+	"/index.html",
+	"/index.js",
+	"/manifest.webmanifest",
+	"/styles.css",
+	"/icons/icon-192x192.png",
+	"/icons/icon-512x512.png",
+	"https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+	"https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
 ];
 
-
-
-
-
-
+const CACHE_NAME = "my-site-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v2";
 
 //pre cache resources when the service worker is installed
+//Install and register service worker
+self.addEventListener("install", function (event) {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then(function (cache) {
+			console.log("Your files were pre-cached successfully!");
+			return cache.addAll(FILES_TO_CACHE);
+		})
+	);
 
-//remove old caches whenj the service worker activates
+	self.skipWaiting();
+});
+
+//remove old caches when the service worker activates
+// The activate handler takes care of cleaning up old caches.
+//Activation
+self.addEventListener("activate", function (event) {
+	event.waitUntil(
+		caches.keys().then((keyList) => {
+			return Promise.all(
+				keyList.map((key) => {
+					if (key !== CACHE_NAME && key != DATA_CACHE_NAME) {
+						console.log("Removing old cache data", key);
+						return caches.delete(key);
+					}
+				})
+			);
+		})
+	);
+
+	self.clients.claim();
+});
 
 // serve files from the cache when not online
+// Enable the service worker to intercept network requests
+self.addEventListener("fetch", function(event) {
+    // Handle requests
+    // Cache responses for requests for data
+    if (event.request.url.includes("/api/") && event.request.method === "GET") {
+        console.log("[Service Worker] Fetch (data)", event.request.url);
+
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                // If it's successful put it in our cache
+                return fetch(event.request)
+                    .then(response => {
+                        if (response.status === 200) {
+                            cache.put(event.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    // If not successful, then pull it from our cache
+                    .catch(err => {
+                        return cache.match(event.request);
+                    });
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        );
+
+        return;
+    }
+
+    // Serve static files from the cache.
+    // Then do a network request when the resource is not in the cache
+    // This code allows the page to be accessible offline**
+    event.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(event.request).then(response => {
+                return response || fetch(event.request);
+            });
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    );
+});
